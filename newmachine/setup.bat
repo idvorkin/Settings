b3fe4fc0d925e3ca2b57b30a141249f3d14cc88b
idvorkin <idvorkin@gmail.com>
2015-02-13 08:00:53 -0800

    Copy from hg repository

---
+++ newmachine/setup.bat
@@ -0,0 +1,30 @@
+mklink %USERPROFILE%\pyreadlineconfig.ini %CD%\pyreadlineconfig.ini
+
+REM Setup dropbox Paths
+        mklink /d \dropbox %USERPROFILE%\dropbox
+        mklink /d \bin_drop %USERPROFILE%\dropbox\bin_drop
+
+REM Setup Vim Paths
+        mklink /d %USERPROFILE%\vimfiles c:\hg\vim\vimfiles
+        mklink %USERPROFILE%\_vimrc c:\hg\vim\default_vimrc
+REM Setup Mercurial Path
+        mklink %USERPROFILE%\Mercurial.ini c:\hg\hg\Mercurial.ini
+
+REM Setup Auto Hot Key Path
+        del %USERPROFILE%\Documents\AutoHotkey.ahk 
+        mklink %USERPROFILE%\Documents\AutoHotkey.ahk c:\hg\autohotkey\AutoHotkey.ahk
+        del %USERPROFILE%\Documents\vim_onenote.ahk
+        mklink %USERPROFILE%\Documents\vim_onenote.ahk c:\hg\autohotkey\vim_onenote.ahk
+
+REM Setup choco stuff
+    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+REM test a current machine with choco list -localonly
+    cinst git
+    cinst nodejs
+    cinst ag
+    cinst conemu
+    cinst gvim
+    cinst nunit
+    cinst nuget
+    cinst repo
