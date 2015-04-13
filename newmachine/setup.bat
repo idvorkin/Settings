db483a3a7eb13ee26f8c26d242b7a96dad1edc43
Igor <idvorkin@gmail.com>
2015-04-13 12:47:06 -0700

    Adding git-post.

--- newmachine/setup.bat
+++ newmachine/setup.bat
@@ -11,11 +11,19 @@ REM Setup Mercurial Path
 REM Setup Auto Hot Key Path
         del %USERPROFILE%\Documents\AutoHotkey.ahk 
         mklink %USERPROFILE%\Documents\AutoHotkey.ahk c:\hg\autohotkey\AutoHotkey.ahk
+
         del %USERPROFILE%\Documents\vim_onenote.ahk
         mklink %USERPROFILE%\Documents\vim_onenote.ahk c:\hg\autohotkey\vim_onenote.ahk
 
+        del %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+        mklink %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 c:\gits\settings\Microsoft.PowerShell_profile.ps1
+
 REM Setup choco stuff
     @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+    REM Install PSGet
+    REM
+    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex (new-object Net.WebClient).DownloadString('http://psget.net/GetPsGet.ps1')"
 
 REM test a current machine with choco list -localonly
     cinst git
@@ -26,3 +34,6 @@ REM test a current machine with choco list -localonly
     cinst nunit
     cinst nuget
     cinst repo
+
+REM Install PS-Get
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Install-Module Posh-Git –force"
