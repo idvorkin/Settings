28f4e5c47d5b73d4169a89d3f6fc0b00aa8add41
Igor Dvorkin <idvorkin@gmail.com>
2015-08-15 05:54:32 -0700

    Adding git setup vim bundle and karabiner docs

--- mac/install.sh
+++ mac/install.sh
@@ -1,7 +1,18 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
-brew install brew-cask	cask		emacs		fish		libevent	openssl		tmux		vim
+brew install brew-cask	cask		emacs		fish		libevent	openssl		tmux		vim ag
 brew cask install google-chrome  iterm2	      karabiner	     seil	    skype
+
+# shared git stetup
+
 git config --global user.email "idvorkin@gmail.com"
 git config --global user.name "Igor Dvorkin"
+git config --global push.default simple
 
+# in seil map caps -> F19 (Keycode: 80) 
+ln ~/settings/private.xml ~/Library/Application\ Support/karabiner/private.xml
+ln ~/settings/_vsvimrc ~/.vsvimrc 
+ln ~/settings/private.xml  ~/.vimrc 
+git clone https://github.com/gmarik/vundle.git ~/vimfiles/bundle/vundle
+
+# 
 
