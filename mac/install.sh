5f963613ab582c3fdf88ef280f6e72aa2316f870
Igor Dvorkin <idvorkin@gmail.com>
2015-08-15 05:37:32 -0700

    Adding mac stuff

---
+++ mac/install.sh
@@ -0,0 +1,7 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
+brew install brew-cask	cask		emacs		fish		libevent	openssl		tmux		vim
+brew cask install google-chrome  iterm2	      karabiner	     seil	    skype
+git config --global user.email "idvorkin@gmail.com"
+git config --global user.name "Igor Dvorkin"
+
+
