fea141d60ad0f15486a312b891dbc0ecb2c5ea66
Igor Dvorkin <idvorkin@gmail.com>
2015-08-21 07:07:03 -0700

    More git stuff

--- mac/install.sh
+++ mac/install.sh
@@ -1,6 +1,6 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 # looks like brew is just for command line stuff.
-brew install brew-cask	cask		emacs		zsh		libevent	openssl		tmux	vim ag 
+brew install brew-cask	cask		emacs		zsh		libevent	openssl		tmux	vim ag  git
 # Looks like we need CASK to do things that are findable.
 brew cask install google-chrome  iterm2	      karabiner	     seil	    skype macvim
 
@@ -9,6 +9,9 @@ brew cask install google-chrome  iterm2	      karabiner	     seil	    skype macv
 git config --global user.email "idvorkin@gmail.com"
 git config --global user.name "Igor Dvorkin"
 git config --global push.default simple
+# Mac only git setup
+git config --global credential.helper osxkeychain
+git config --global core.editor /usr/bin/vim
 
 # in seil map caps -> F19 (Keycode: 80) 
 ln ~/settings/private.xml ~/Library/Application\ Support/karabiner/private.xml
@@ -36,3 +39,5 @@ brew install ctags --HEAD
 
 #Alacatraz
 curl -fsSL https://raw.github.com/supermarin/Alcatraz/master/Scripts/install.sh | sh
+
+# Git setup for newer git
