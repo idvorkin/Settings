a84a261db7b3883baefd9d06fef3d7cd854cc951
Igor Dvorkin <idvorkin@gmail.com>
2015-11-22 14:01:44 -0800

    Adding alcatraz

---
+++ mac/alcatraz.sh
@@ -0,0 +1,8 @@
+# from: https://github.com/alcatraz/Alcatraz/issues/332
+# doesn't exist - alcatraz install package
+
+# but go to alcatraz. in UX
+# Windows -> Package Manager -> 
+    # ZoomWin
+    # XVim
+
