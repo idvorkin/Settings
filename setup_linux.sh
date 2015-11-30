7f5024525e3d66b699f34aef66a9e794de4e0fc7
Igor Dvorkin <idvorkin@gmail.com>
2015-11-29 20:04:25 -0800

    Adding nodejs and cleaning up get setup instructions.

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,8 +1,8 @@
-sudo apt-get install virtualbox-guest-additions-iso vim-gnome vim git zsh ruby curl ruby-dev zlib1g-dev
-cd ~
-git clone https://github.com/idvorkin/settings
-mkdir gits
-git clone https://github.com/idvorkin/idvorkin.github.io
+# assume already cloned settings.
+sudo apt-get install virtualbox-guest-additions-iso vim-gnome vim git zsh ruby curl ruby-dev zlib1g-dev nodejs
+mkdir ~/gits
+ln -s ~/settings ~/gits/settings
+git clone https://github.com/idvorkin/idvorkin.github.io ~/gits/idvorkin.github.io
 
 #Setup OhMyZSH
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
