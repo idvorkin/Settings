0a1d75db6568826de26481d5867d9c1cb2abce7c
Igor <idvorkin@gmail.com>
2016-02-07 06:40:28 -0800

    Fix logc alias.

--- newmachine/setup.bat
+++ newmachine/setup.bat
@@ -64,7 +64,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.logc git log master..
+git config --global alias.logc master..
 
 REM Setup policy execution policy
 powershell Set-ExecutionPolicy RemoteSigned
