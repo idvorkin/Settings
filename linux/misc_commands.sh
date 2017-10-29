140f9da0101f8bfabd83818cd3f7a1e80a2a0fa1
Igor Dvorkin <idvorkin@gmail.com>
2017-10-28 18:18:12 -0700

    How to set default protocol handers (handy for magnet links)

--- linux/misc_commands.sh
+++ linux/misc_commands.sh
@@ -6,3 +6,9 @@ dpkg-query -W --showformat='${Installed-Size;10}\t${Package}\n' | sort -k1,1n
 # Mount the host filesystem in ~/etc/rc.d
 # mount -t vboxsf share /home/ubuntu/host
 sudo mount -t vboxsf -o rw,uid=1000,gid=1000 linux ~/host
+
+# Register protocol handlers
+# http://kb.mozillazine.org/Register_protocol
+# set to false to cause it to be autoselected next time.
+
+gconftool-2 -s /desktop/gnome/url-handlers/foo/enabled --type Boolean false
