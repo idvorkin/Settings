8f263dc16ff7b17cb2943533eebc4b7bf00a4e87
Igor Dvorkin <idvorkin@gmail.com>
2018-09-23 14:50:58 +0000

    Update to read and use the template

--- vim_python.py
+++ vim_python.py
@@ -3,22 +3,34 @@ import vim
 from datetime import datetime
 from os import path
 import pathlib
+import shutil
 
 
 def MakeDailyPage():
     # Todo make this be path relative so works on unix.
     date_in_format = datetime.now().strftime("%Y-%m-%d")
     fileName = path.join(
-        path.expanduser("~"), "gits/igor2/750words/" + date_in_format + ".md"
+        path.expanduser("~"), f"gits/igor2/750words/{date_in_format}.md"
     )
-    if path.isfile(fileName):
+    templateFileName  = path.join(
+        path.expanduser("~"), "gits/igor2/750words/daily_template.md"
+    )
+    isAlreadyCreated = path.isfile(fileName)
+
+    if isAlreadyCreated:
         print(fileName + " exists")
     else:
+        print(f"output: {fileName}")
+        print(f"template: {templateFileName}")
         print(fileName)
-        with open(fileName, "w") as f:
-            f.write("750 words for:" + date_in_format + "\n\n\n\n")
+        shutil.copyfile(templateFileName, fileName)
+
     vim.command("next " + fileName)
     vim.command("lcd %:p:h")  # Goto current Directory
     vim.command("IGWriteOn")
     vim.command("Gwrite")
-    vim.command("999999")  # Goto last line.
+
+    if not isAlreadyCreated:
+        vim.command(f"%s/!template_date!/{date_in_format}/")  # Goto first line
+
+    vim.command("9999")  # Goto last line.
