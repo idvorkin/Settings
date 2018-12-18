dab77e166054daf2f5cc9ddf2795126db49155e8
Igor Dvorkin <idvorkin@gmail.com>
2018-12-18 09:16:19 -0800

    Adding azure-cli

--- shared/shared_install.sh
+++ shared/shared_install.sh
@@ -1,6 +1,6 @@
 # goto http://linuxbrew.sh/
 # looks like brew is just for command line stuff.
-brew install cask zsh libevent openssl tmux	ag  git wget ncdu graphviz htop python3 ranger cmatrix diff-so-fancy grv fzf bat fd ruby iftop aws-shell
+brew install cask zsh libevent openssl tmux	ag  git wget ncdu graphviz htop python3 ranger cmatrix diff-so-fancy grv fzf bat fd ruby iftop aws-shell azure-cli
 
 # ranger = File Explorer
 # grv - get repository viewer
