a9272407832c5ce4bfb74d34760620c9d688b987
Igor Dvorkin <idvorkin@gmail.com>
2019-01-03 05:38:30 +0000

    Have template file replacement done via python. Prep for TZ support

--- vim_python.py
+++ vim_python.py
@@ -1,13 +1,10 @@
 import vim
 from datetime import datetime, timedelta
 from os import path
-import shutil
 
 
 def pathBasedAtIgor2(filepath):
-    return path.join(
-        path.expanduser("~"), f"gits/igor2/{filepath}"
-    )
+    return path.join(path.expanduser("~"), f"gits/igor2/{filepath}")
 
 
 def MakeTemplatePage(date, directory, template_name):
@@ -19,10 +16,15 @@ def MakeTemplatePage(date, directory, template_name):
     if isAlreadyCreated:
         print(fileName + " exists")
     else:
+        with open(templateFileName, "r") as fileTemplate:
+            content = fileTemplate.read()
+            content.replace("!template_date!", date_in_format)
+            with open(fileName, "w") as fileWrite:
+                fileWrite.write(content)
+
         print(f"output: {fileName}")
         print(f"template: {templateFileName}")
         print(fileName)
-        shutil.copyfile(templateFileName, fileName)
 
     vim.command("next " + fileName)
     vim.command("lcd %:p:h")  # Goto current Directory
@@ -36,12 +38,19 @@ def MakeTemplatePage(date, directory, template_name):
     return
 
 
+def NowPST():
+    # groan no timzone in standard library so fake it - subtract nope, don't want to
+    # subtract as it won't work for non standard times! Cripes - how annoying.
+    # return datetime.now().astimezone(timezone("US/Pacific"))
+    return datetime.now()
+
+
 def MakeDailyPage():
-    MakeTemplatePage(datetime.now(), "750words", "daily_template")
+    MakeTemplatePage(NowPST(), "750words", "daily_template")
 
 
 def MakeWeeklyReport():
-    now = datetime.now().date()
+    now = NowPST()
     startOfWeek = now - timedelta(days=now.weekday())
 
     # Make to sart of week.
