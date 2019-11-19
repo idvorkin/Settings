45a82481367536ed2cfded47d9c5f03665420541
Igor Dvorkin <idvorkin@gmail.com>
2019-11-19 15:01:30 +0000

    Add dumping graetful

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -4,4 +4,18 @@ PATH+=:~/.local/bin
 alias gfrall='for git_directory in * ; echo $git_directory && git -C $git_directory fr'
 alias dwc='pushd ~/gits/settings && python3 -c "from vim_python import * ;WCDailyPage()" && pushd ~/gits/igor2/750words '
 alias dgc='pushd ~/gits/settings && python3 -c "from vim_python import * ;GitCommitDailyPage()" && pushd ~/gits/igor2/750words '
+
+unalias ddg
+function ddg() {
+    pushd ~/gits/linqpadsnippets/python
+    python3 dump_grateful.py $1
+    popd
+}
+function dda() {
+    pushd ~/gits/linqpadsnippets/python
+    python3 dump_grateful.py --awesome $1
+    popd
+}
+
+
 set -o vi
