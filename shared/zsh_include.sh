7e5d5a391fa3108ee70bae111fca812800f20df3
Igor Dvorkin <idvorkin@gmail.com>
2019-11-29 15:39:53 +0000

    Add gpushall

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -2,6 +2,7 @@
 [ -f ~/gits/z/z.sh ]  && source ~/gits/z/z.sh
 PATH+=:~/.local/bin
 alias gfrall='for git_directory in * ; echo $git_directory && git -C $git_directory fr'
+alias gpushall='for git_directory in * ; echo $git_directory && git -C $git_directory push'
 alias dwc='pushd ~/gits/settings && python3 -c "from vim_python import * ;WCDailyPage()" && pushd ~/gits/igor2/750words '
 alias dgc='pushd ~/gits/settings && python3 -c "from vim_python import * ;GitCommitDailyPage()" && pushd ~/gits/igor2/750words '
 
