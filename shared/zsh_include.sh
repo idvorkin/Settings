bb7243f1f5f4f4fb0b89d30631f51b97dfd14437
Igor Dvorkin <idvorkin@gmail.com>
2020-01-01 05:26:39 +0000

    Adding zsh shared goop.

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -1,10 +1,14 @@
-# shared zsh settings to be sourced
+##
+#
+export PATH=/home/linuxbrew/.linuxbrew/bin/:$PATH
 [ -f ~/gits/z/z.sh ]  && source ~/gits/z/z.sh
 PATH+=:~/.local/bin
 alias gfrall='for git_directory in * ; echo $git_directory && git -C $git_directory fr'
 alias gpushall='for git_directory in * ; echo $git_directory && git -C $git_directory push'
 alias dwc='pushd ~/gits/settings && python3 -c "from vim_python import * ;WCDailyPage()" && pushd ~/gits/igor2/750words '
 alias dgc='pushd ~/gits/settings && python3 -c "from vim_python import * ;GitCommitDailyPage()" && pushd ~/gits/igor2/750words '
+
+alias tmuxp="~/.local/bin/tmuxp"
 
 unalias ddg
 function ddg() {
@@ -27,3 +31,17 @@ function dda() {
 
 
 set -o vi
+# shared zsh settings to be sourced
+# TMUX attach
+#
+# if [ -z "$TMUX" ]; then
+        # tmux attach -t main || tmuxp load simple
+# fi
+#
+# Storing other just in case stuff
+# eval "$(fasd --init auto)"
+# export PATH=$PATH:$HOME/dotnet
+# export LANGUAGE=en_US.UTF-8
+# export LANG=en_US.UTF-8
+# export LC_ALL=en_US.UTF-8
+# export PATH="$PATH:$HOME/.dotnet/tools"
