0ce13eeafe96fdee20bd76e2580ce17783c67db9
Igor Dvorkin <idvorkin@gmail.com>
2020-01-01 06:02:40 +0000

    Remove pre-commit hooks

--- shared/cron_git_sync.sh
+++ shared/cron_git_sync.sh
@@ -2,7 +2,10 @@
 #  copied from zsh_include
 #  Run cron command every 4 minutes
 # */4 * * * * ~/settings/shared/cron_git_sync.sh >>~/tmp/cronrun 2>&1
+echo running
 date
 cd ~/gits/
-for git_directory in * ; echo $git_directory && git -C $git_directory fr
-for git_directory in * ; echo $git_directory && git -C $git_directory push
+echo fetching ...
+for git_directory in * ; echo $git_directory && git -C $git_directory fr --no-verify
+echo pushing ..
+for git_directory in * ; echo $git_directory && git -C $git_directory push --no-verify
