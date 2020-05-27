d9d042c8c2069d5cd318de395aad7f1df92a0495
Igor Dvorkin <idvorkin@gmail.com>
2020-05-27 07:48:56 -0700

    Save install.sh

--- mac/install.sh
+++ mac/install.sh
@@ -1,26 +1,17 @@
-# Ruby
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
 
-# Linux -- 
-
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-
-brew install macvim 
+brew install macvim
 
 # run shared install here.
 
 # Install cask
 brew install caskroom/cask/brew-cask
-# Looks like we need CASK to do things that are findable. 
+# Looks like we need CASK to do things that are findable.
 brew cask install google-chrome  iterm2	skype spectacle sourcetree virtualbox vagrant macdown the-unarchiver anaconda
-brew cask install visual-studio-code hammerspoon sourcetree 
+brew cask install visual-studio-code hammerspoon sourcetree
 brew cask install osxfuse
 brew cask install sshfs vlc
 
 
-# window manager -- pretty cool 
-# https://www.spectacleapp.com/
-brew cask install spectacle
 
 brew linkapps macvim
 
@@ -29,16 +20,12 @@ git config --global credential.helper osxkeychain
 git config --global core.editor /usr/local/bin/vim
 
 
-# Setup vundle
-git clone https://github.com/gmarik/vundle.git ~/vimfiles/bundle/vundle
 # setup xcode
 git clone https://github.com/XVimProject/XVim.git  ~/gits/XVim
 cd ~/gits/XVim
 #make
 
 
-# Jekyll
-# gem install sudo gem install -n /usr/local/bin/ jekyll jekyll-paginate
-
-#Alacatraz
-curl -fsSL https://raw.github.com/supermarin/Alcatraz/master/Scripts/install.sh | sh
+# window manager -- pretty cool
+# https://www.spectacleapp.com/
+brew cask install spectacle
