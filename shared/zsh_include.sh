9e41efc3a77e2d14cada688138db1cdeee5675fa
Igor Dvorkin <idvorkin@gmail.com>
2020-08-31 06:32:35 -0700

    Add alias for remote pbcopy and ssh lightsail

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -34,7 +34,8 @@ alias gpushall='for git_directory in * ; echo $git_directory && git -C $git_dire
 alias weather="curl wttr.in/seattle"
 alias dwc='pushd ~/gits/settings && python3 -c "from vim_python import * ;WCDailyPage()" && pushd ~/gits/igor2/750words '
 alias dgc='pushd ~/gits/settings && python3 -c "from vim_python import * ;GitCommitDailyPage()" && pushd ~/gits/igor2/750words '
-alias sshs='ssh lightsail'
+alias sl='ssh lightsail'
+alias rpbcopy='nc localhost 5726'
 
 alias tmuxp="~/.local/bin/tmuxp"
 
