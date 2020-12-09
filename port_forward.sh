8c09768406fba90ba938a7433b429cd9cdcac58f
Igor Dvorkin <idvorkin@gmail.com>
2020-12-09 15:20:02 +0000

    Save port_forward.sh

--- port_forward.sh
+++ port_forward.sh
@@ -1,3 +1,4 @@
 # Some problem with SSH interactive mode, use mosh and seperate ssh instance for port forwarding
-ssh lightsail /home/ec2-user/homebrew/bin/nc 80
+# This script uses lightsail which has port forwarding, and a blocking command with no output
+ssh lightsail /home/ec2-user/homebrew/bin/nc -l -p 1201
 
