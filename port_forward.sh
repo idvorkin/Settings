7a23cfb7d6f0459dd2b78d581b86df5fbc13ccff
Igor Dvorkin <idvorkin@gmail.com>
2021-03-28 09:50:03 -0700

    Update port forward

--- port_forward.sh
+++ port_forward.sh
@@ -2,8 +2,8 @@
 # This script uses lightsail which has port forwarding, and a blocking command with no output
 
 # I just need a command that blocks, but it becomes a zombie, so need to be careful about leaving it around
-# Block for 2 hours
+# Block for 20 hours
 echo Running ssh sleep
-ssh lightsail /usr/bin/sleep 3600
+ssh lightsail /usr/bin/sleep 36000
 echo Done ssh sleeping
 
