27223467b159c280606cab8d600f1fc44f6a5811
Igor Dvorkin <idvorkin@gmail.com>
2022-08-06 08:07:35 -0700

    Save brew_packages.sh

--- shared/brew_packages.sh
+++ shared/brew_packages.sh
@@ -17,6 +17,8 @@ dua-cli \
 ranger \
 cask \
 fasd \
+docker \
+docker-compose \
 diff-so-fancy \
 git-extras \
 git-delta \
@@ -96,4 +98,4 @@ npm install --global fkill-cli
 pip3 install install black\[jupyter\] arrow numpy pytz loguru typer openai
 
 
-brew install --cask karabiner-elements iterm2 alfred bartender alt-tab hammerspoon 
+brew install --cask karabiner-elements iterm2 alfred bartender alt-tab hammerspoon  visual-studio-code
