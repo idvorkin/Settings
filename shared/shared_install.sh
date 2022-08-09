4b4f31bb3d58323bb2fa947e22048583b1113155
Igor Dvorkin <idvorkin@gmail.com>
2022-08-09 10:53:45 -0700

    Add litecli config for vi

--- shared/shared_install.sh
+++ shared/shared_install.sh
@@ -28,6 +28,7 @@ popd
 # Link to lots of dot files
 ln -s -f ~/settings/mac/karabiner.json ~/.config/karabiner/karabiner.json
 ln -s -f ~/settings/default_vimrc ~/.vimrc
+ln -s -f ~/settings/shared/litecli_config ~/.config/litecli/config
 ln -s -f ~/settings/mac/.xvimrc ~/.xvimrc
 ln -s -f ~/settings/mac/.inputrc ~/.inputrc
 ln -s -f ~/settings/tmuxp ~/.tmuxp
