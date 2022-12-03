86bfd2324cc1de4502c61b878eb26b666e892446
Igor Dvorkin <idvorkin@gmail.com>
2022-12-03 08:37:25 -0800

    Save forcepy.sh

--- forcepy.sh
+++ forcepy.sh
@@ -1,1 +1,4 @@
+# Sigh, sometimes you need to fix openssl when you get SSL errors
+# brew reinstall openssl@1.1
+# pip3 install setuptools_rust
 brew unlink python@3.10 && brew link --overwrite python@3.10
