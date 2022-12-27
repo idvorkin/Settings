426340ef9c91e239b79ce8bb5e5e0e28e4dae37c
Igor Dvorkin <idvorkin@gmail.com>
2022-12-27 11:27:54 -0800

    Save brew_packages.sh

--- shared/brew_packages.sh
+++ shared/brew_packages.sh
@@ -103,6 +103,11 @@ npm install --global fkill-cli
 # Add some pip3 packages
 python3 -m pip install install black\[jupyter\] arrow numpy pytz loguru typer openai icecream jupyterlab typer rich seaborn matplotlib pandas ipywidgets altair sklearn plotly pyfiglet jsonpickle pandas-datareader nltk fernet cryptography torch opencv-python imutils sklearn torchvision scikit-learn
 
+
+# On the mac can install tensor flow as follows
+python3 -m pip install tensorflow-macos
+
+
 # what is the obs plugin
 
 brew install --cask karabiner-elements iterm2 alfred bartender alt-tab hammerspoon  visual-studio-code docker vlc
