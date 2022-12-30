7a4b2b857cc83cd1439947360a6f9320b1655054
Igor Dvorkin <idvorkin@gmail.com>
2022-12-30 17:47:04 +0000

    Save zsh_include.sh

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -35,7 +35,12 @@ alias weather="curl wttr.in/seattle"
 alias dwc='pushd ~/gits/settings && python3 -c "from vim_python import * ;WCDailyPage()" && pushd ~/gits/igor2/750words '
 alias dgc='pushd ~/gits/settings && python3 -c "from vim_python import * ;GitCommitDailyPage()" && pushd ~/gits/igor2/750words '
 alias sl='ssh lightsail'
-alias rpbcopy='nc localhost 5726'
+alias rpbcopy='~/settings/rpbcopy.sh'
+alias rpbpaste='~/settings/rpbpaste.sh'
+alias rpbc='~/settings/rpbcopy.sh'
+alias rpbp='~/settings/rpbpaste.sh'
+alias pbc='pbcopy'
+alias pbp='pbpaste'
 
 alias tmuxp="~/.local/bin/tmuxp"
 alias mb="pbpaste | sed  's!idvork.in/!idvorkin.azurewebsites.net/!'| sed 's!#!/!' | pbcopy"
