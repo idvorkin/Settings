d5bbec749df16b4c1313184fec6133904a9ef149
Igor Dvorkin <idvorkin@gmail.com>
2022-12-31 20:27:06 +0000

    Save zsh_include.sh

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -35,10 +35,12 @@ alias weather="curl wttr.in/seattle"
 alias dwc='pushd ~/gits/settings && python3 -c "from vim_python import * ;WCDailyPage()" && pushd ~/gits/igor2/750words '
 alias dgc='pushd ~/gits/settings && python3 -c "from vim_python import * ;GitCommitDailyPage()" && pushd ~/gits/igor2/750words '
 alias sl='ssh lightsail'
-alias rpbcopy='~/settings/rpbcopy.sh'
+
+# Looks like pbcopy just works over ssh/mosh now!
+# alias rpbcopy='~/settings/rpbcopy.sh'
 alias rpbpaste='~/settings/rpbpaste.sh'
 # alias rpbc='~/settings/rpbcopy.sh'
-alias rpbc='~/settings/pbcopy'
+# alias rpbc='~/settings/pbcopy'
 alias rpbp='~/settings/rpbpaste.sh'
 alias pbc='pbcopy'
 alias pbp='pbpaste'
