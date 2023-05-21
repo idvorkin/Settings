c406d24835a8235b1a68455b13d90df5b037fbce
Igor Dvorkin <idvorkin@gmail.com>
2023-05-21 07:27:37 -0700

    Adding iterm image utils

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -29,6 +29,7 @@ source_if_exists ~/.fzf.zsh
 source_if_exists ~/homebrew/etc/profile.d/z.sh
 
 PATH+=:~/.local/bin
+alias imgls='timg --grid 4 --title'
 alias gfrall='for git_directory in * ; echo $git_directory && git -C $git_directory fr'
 alias gpushall='for git_directory in * ; echo $git_directory && git -C $git_directory push'
 alias weather="curl wttr.in/seattle"
