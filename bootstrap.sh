58e57bfd084dd0491fef9a7169825d267954fb86
Igor Dvorkin <idvorkin@gmail.com>
2023-05-28 06:24:37 -0700

    Move some of the fast commands to bootstrap so dev containers can set them up

---
+++ bootstrap.sh
@@ -0,0 +1,58 @@
+
+# This is used by vs codespaces to setup....
+# Should be fast
+
+# load up the git repo's for plugin managers
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# shared git stetup
+# Hymn, maybe i have this in a git config file instead?
+git config --global user.email "idvorkin@gmail.com"
+git config --global user.name "Igor Dvorkin"
+git config --global push.default simple
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "yellow"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+git config --global push.default simple
+git config --global alias.co checkout
+git config --global alias.com "checkout master"
+git config --global alias.fr "pull --rebase"
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.logc log master..
+git config --global alias.logp "log --pretty=format:'%C(yellow)%h%Cred%d %Creset%s %C(yellow)[%cn] %C(green)(%ar)' --decorate"
+
+# Link to lots of dot files
+mkdir ~/.config/karabiner/
+ln -s -f ~/settings/mac/karabiner.json ~/.config/karabiner/karabiner.json
+ln -s -f ~/settings/mac/multi_keyboard_sync.json ~/.config/karabiner/assets/complex_modifications/multi_keyboard_sync.json
+ln -s -f ~/settings/default_vimrc ~/.vimrc
+ln -s -f ~/settings/shared/ranger_rc.conf ~/.config/ranger/rc.conf
+ln -s -f ~/settings/shared/litecli_config ~/.config/litecli/config
+ln -s -f ~/settings/mac/.xvimrc ~/.xvimrc
+ln -s -f ~/settings/mac/.inputrc ~/.inputrc
+ln -s -f ~/settings/tmuxp ~/.tmuxp
+mkdir ~/.hammerspoon
+mkdir ~/.ssh
+
+ln -s -f ~/settings/mac/hammerspoon/init.lua ~/.hammerspoon/init.lua
+ln -s -f ~/settings/mac/.finicky.js  ~/.finicky.js
+ln -s -f ~/settings/shared/ctags ~/.ctags
+ln -s -f ~/settings/shared/.tmux.conf ~/.tmux.conf
+ln -s -f ~/settings/shared/.vimperatorrc ~/.vimperatorrc
+ln -s -f ~/settings/shared/ipython_config.py  ~/.ipython/ipython_config.py
+ln -s -f ~/settings/shared/ipython_config.py  ~/.ipython/profile_default/ipython_config.py
+ln -s -f ~/settings/shared/.ideavim ~/.ideavimrc
+ln -s -f ~/settings/shared/ssh_config ~/.ssh/config
--- shared/shared_install.sh
+++ shared/shared_install.sh
@@ -7,44 +7,11 @@ mkdir ~/homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | ta
 # Run the install script.
 . ./brew_packages.sh
 
-# load up the git repo's for plugin managers
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# shared git stetup
-git config --global user.email "idvorkin@gmail.com"
-git config --global user.name "Igor Dvorkin"
-git config --global push.default simple
+. ~/settings/bootrap.sh
 
-# Personal respoistories
-mkdir ~/gits
-cd ~/gits
-git clone https://github.com/idvorkin/LinqpadSnippets
-git clone https://github.com/idvorkin/idvorkin.github.io
-git clone https://github.com/idvorkin/jupyter
-git clone https://idvorkin@bitbucket.org/idvorkin/igor2.git
-popd
-
-# Link to lots of dot files
-mkdir ~/.config/karabiner/
-ln -s -f ~/settings/mac/karabiner.json ~/.config/karabiner/karabiner.json
-ln -s -f ~/settings/mac/multi_keyboard_sync.json ~/.config/karabiner/assets/complex_modifications/multi_keyboard_sync.json
-ln -s -f ~/settings/default_vimrc ~/.vimrc
-ln -s -f ~/settings/shared/ranger_rc.conf ~/.config/ranger/rc.conf
-ln -s -f ~/settings/shared/litecli_config ~/.config/litecli/config
-ln -s -f ~/settings/mac/.xvimrc ~/.xvimrc
-ln -s -f ~/settings/mac/.inputrc ~/.inputrc
-ln -s -f ~/settings/tmuxp ~/.tmuxp
-mkdir ~/.hammerspoon
+# Some private dot files
 mkdir ~/.ssh
-ln -s -f ~/settings/mac/hammerspoon/init.lua ~/.hammerspoon/init.lua
-ln -s -f ~/settings/mac/.finicky.js  ~/.finicky.js
-ln -s -f ~/settings/shared/ctags ~/.ctags
-ln -s -f ~/settings/shared/.tmux.conf ~/.tmux.conf
-ln -s -f ~/settings/shared/.vimperatorrc ~/.vimperatorrc
-ln -s -f ~/settings/shared/ipython_config.py  ~/.ipython/ipython_config.py
-ln -s -f ~/settings/shared/ipython_config.py  ~/.ipython/profile_default/ipython_config.py
-ln -s -f ~/settings/shared/.ideavim ~/.ideavimrc
 ln -s -f ~/settings/shared/ssh_config ~/.ssh/config
 
 # Not quite ready to dick iwith it.
@@ -101,30 +68,6 @@ git config --global receive.denyCurrentBranch updateInstead
 # https://github.com/so-fancy/diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
-git config --global color.ui true
-
-git config --global color.diff-highlight.oldNormal    "red bold"
-git config --global color.diff-highlight.oldHighlight "red bold 52"
-git config --global color.diff-highlight.newNormal    "green bold"
-git config --global color.diff-highlight.newHighlight "green bold 22"
-
-git config --global color.diff.meta       "yellow"
-git config --global color.diff.frag       "magenta bold"
-git config --global color.diff.commit     "yellow bold"
-git config --global color.diff.old        "red bold"
-git config --global color.diff.new        "green bold"
-git config --global color.diff.whitespace "red reverse"
-git config --global user.email "idvorkin@gmail.com"
-git config --global user.name "Igor Dvorkin"
-git config --global push.default simple
-git config --global alias.co checkout
-git config --global alias.com "checkout master"
-git config --global alias.fr "pull --rebase"
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
-git config --global alias.logc log master..
-git config --global alias.logp "log --pretty=format:'%C(yellow)%h%Cred%d %Creset%s %C(yellow)[%cn] %C(green)(%ar)' --decorate"
 
 # Now I run jekyll in a container -- w00t!
 
