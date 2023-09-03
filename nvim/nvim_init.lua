421fb60a5ab5122afb908e3b0d6ae98ade259179
Igor Dvorkin <idvorkin@gmail.com>
2023-09-03 15:49:57 -0700

    Add cmp completer

--- nvim/nvim_init.lua
+++ nvim/nvim_init.lua
@@ -158,7 +158,22 @@ require('gitsigns').setup{
 -- Setup cmp
 --
 local cmp = require'cmp'
-cmp.setup()
+cmp.setup{
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+
+}
+
+
 -- `/` cmdline setup.
 cmp.setup.cmdline('/', {
   mapping = cmp.mapping.preset.cmdline(),
