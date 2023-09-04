52b2ae43b2bb5bfdc7b9e07e0c7add704990bdca
Igor Dvorkin <idvorkin@gmail.com>
2023-09-04 04:07:44 +0000

    Make it easier to build python packages

--- shared/brew_packages.sh
+++ shared/brew_packages.sh
@@ -123,7 +123,8 @@ brew install azure-cli cmatrix iftop
 npm install --global fkill-cli
 
 # Add some pip3 packages
-python3 -m pip install install black\[jupyter\] arrow numpy pytz loguru typer openai icecream jupyterlab typer rich seaborn matplotlib pandas ipywidgets altair sklearn plotly pyfiglet jsonpickle pandas-datareader nltk fernet cryptography imutils sklearn scikit-learn jupyterlab-vim jupyterlab-vimrc pendulum poetry torch torchvision opencv-python typeguard tiktoken modal-client uvicorn py-cord asyncify asyncer mypy langchain unstructured chromadb wandb pudb
+python3 -m pip install install black\[jupyter\] arrow numpy pytz loguru typer openai icecream jupyterlab typer rich seaborn matplotlib pandas ipywidgets altair plotly pyfiglet jsonpickle pandas-datareader nltk fernet cryptography imutils scikit-learn jupyterlab-vim jupyterlab-vimrc pendulum poetry  typeguard tiktoken modal-client uvicorn py-cord asyncify asyncer mypy langchain unstructured chromadb wandb pudb tenaccity
+python3 -m pip install torch torchvision opencv-python
 
 
 # On the mac can install tensor flow as follows
