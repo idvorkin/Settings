8cfd37069733b12b0ea70fb1e8536c19b55b0d47
Igor Dvorkin <idvorkin@gmail.com>
2023-09-04 18:17:05 +0000

    Checkpoint

--- nvim/nvim_init.lua
+++ nvim/nvim_init.lua
@@ -321,11 +321,11 @@ function GitCommitAndPush()
     print(current_file)
 
     -- Create a new buffer and run git diff in a terminal in that buffer
-    vim.cmd('vnew')
+    vim.cmd('new')
     vim.bo.buftype='nofile'
     vim.bo.bufhidden='hide'
     vim.bo.swapfile=false
-    vim.cmd('terminal git diff --staged ' .. current_file)
+   vim.cmd('terminal git diff --staged ' .. current_file)
     vim.api.nvim_buf_set_keymap(0, 'n', 'q', ':q<CR>', {noremap = true, silent = true})
 
     -- Defining a global function within GitCommitAndPush() to make a closure
@@ -333,7 +333,7 @@ function GitCommitAndPush()
         -- Ask for commit
         local commit_confirm = vim.fn.input('Do you want to commit '.. current_file .. '? (y/n): ')
         local commit_message = "Checkpoint ".. current_file
-        if commit_confirm == '' then
+        if commit_confirm == '' or commit_confirm == 'y' then
             vim.cmd('!git commit '..current_file ..  ' -m '..commit_message..' ' )
             vim.cmd('!git push')
         end
