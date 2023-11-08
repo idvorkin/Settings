72ce3686affb25661513ab049311062d11d18ae6
Igor Dvorkin <idvorkin@gmail.com>
2023-11-08 06:52:10 -0800

    Checkpoint nvim_lualine.lua

--- nvim/nvim_lualine.lua
+++ nvim/nvim_lualine.lua
@@ -7,13 +7,11 @@ local function is_markdown()
 end
 
 -- See themes at https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-require('lualine').setup{
-    options= {theme= 'gruvbox_dark'}
-}
 require("lualine").setup({
   sections = {
-      lualine_x = {
+      lualine_z = {
           "aerial" ,
+          -- By default, Z is line:column, I don't mind line, but column is too noisy
           { wordcount,   cond = is_markdown },
       }
   },
