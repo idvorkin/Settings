7996339ae53100df46f8115f4a984cf22996a207
Igor Dvorkin <idvorkin@gmail.com>
2023-11-26 18:10:08 +0000

    Add color files

---
+++ nvim/nvim_color.lua
@@ -0,0 +1,8 @@
+
+-- Ugh, and down the rabit hole we go!
+-- I need more contrast on the dashbar
+vim.cmd(":colorscheme catppuccin-macchiato")
+
+  options = {
+      -- ... the rest of your lualine config
+  }
