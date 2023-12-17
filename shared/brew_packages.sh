89a008ee8c773bc2d96f7014841326bcb4308c8e
Igor Dvorkin <idvorkin@gmail.com>
2023-12-17 09:46:24 -0800

    Checkpoint brew_packages.sh

--- shared/brew_packages.sh
+++ shared/brew_packages.sh
@@ -127,7 +127,7 @@ brew install azure-cli cmatrix iftop
 npm install --global fkill-cli
 
 # Add some pip3 packages
-python3 -m pip install install black\[jupyter\] arrow numpy pytz loguru typer openai icecream jupyterlab jupytext typer rich seaborn matplotlib pandas ipywidgets altair plotly pyfiglet jsonpickle pandas-datareader nltk fernet cryptography imutils scikit-learn jupyterlab-vim jupyterlab-vimrc pendulum poetry  typeguard tiktoken modal-client uvicorn py-cord asyncify asyncer mypy langchain unstructured chromadb wandb pudb tenacity sparklines
+python3 -m pip install install black\[jupyter\] arrow numpy pytz loguru typer openai icecream jupyterlab jupytext typer rich seaborn matplotlib pandas ipywidgets altair plotly pyfiglet jsonpickle pandas-datareader nltk fernet cryptography imutils scikit-learn jupyterlab-vim jupyterlab-vimrc pendulum poetry  typeguard tiktoken modal-client uvicorn py-cord asyncify asyncer mypy langchain unstructured chromadb wandb pudb tenacity sparklines ruff pre-commit
 
 python3 -m pip install torch torchvision opencv-python
 python3 -m pip install jupyterlab-lsp python-lsp-server
