b44441db48a2cd512050bbdb56fb22e9ae29d94e
Igor Dvorkin <idvorkin@gmail.com>
2024-01-20 09:48:18 -0800

    Adding chatgpt plugi

--- nvim/nvim_plugins.lua
+++ nvim/nvim_plugins.lua
@@ -75,31 +75,23 @@ local plugins = {
     "ekalinin/Dockerfile.vim",
     "terrastruct/d2-vim",
     "voldikss/vim-floaterm",
-    -- gpt plugin
+    -- Other gpT plugin
     {
-        "robitx/gp.nvim",
-        opts = {
-            chat_topic_gen_model = "gpt-4-1106-preview",
-            openai_api_key = get_openai_api_key(),
-            myagents = {
-                {
-                    name = "ChatGPT4",
-                    chat = true,
-                    command = false,
-                    -- string with model name or table with model name and parameters
-                    model = "gpt-4-1106-preview",
-                    -- system prompt (use this to specify the persona/role of the AI)
-                    system_prompt = "You are a general AI assistant.\n\n"
-                    .. "The user provided the additional info about how they would like you to respond:\n\n"
-                    .. "- If you're unsure don't guess and say you don't know instead.\n"
-                    .. "- Ask question if you need clarification to provide better answer.\n"
-                    .. "- Think deeply and carefully from first principles step by step.\n"
-                    .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-                    .. "- Use Socratic method to improve your thinking and coding skills.\n"
-                    .. "- Don't elide any code from your output if the answer requires coding.\n"
-                    .. "- Take a deep breath; You've got this!\n",
-                },
-            }
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+         -- model = "gpt-4-1106-preview",
+        config = function()
+            require("chatgpt").setup({
+            -- openai_api_key = get_openai_api_key(),
+            -- api_key_command = get_openai_api_key(),
+            -- string with model name or table with model name and parameters
+           --  model = "gpt-4-1106-preview",
+            })
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim"
         }
     },
 
@@ -307,6 +299,35 @@ plugins = appendTables(plugins, {
     "Olical/conjure",
     "PaterJason/cmp-conjure",
 })
+    -- gpt plugin
+    local gpt2 =  {
+        {
+            "robitx/gp.nvim",
+            opts = {
+                chat_topic_gen_model = "gpt-4-1106-preview",
+                openai_api_key = get_openai_api_key(),
+                myagents = {
+                    {
+                        name = "ChatGPT4",
+                        chat = true,
+                        command = false,
+                        -- string with model name or table with model name and parameters
+                        model = "gpt-4-1106-preview",
+                        -- system prompt (use this to specify the persona/role of the AI)
+                        system_prompt = "You are a general AI assistant.\n\n"
+                        .. "The user provided the additional info about how they would like you to respond:\n\n"
+                        .. "- If you're unsure don't guess and say you don't know instead.\n"
+                        .. "- Ask question if you need clarification to provide better answer.\n"
+                        .. "- Think deeply and carefully from first principles step by step.\n"
+                        .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+                        .. "- Use Socratic method to improve your thinking and coding skills.\n"
+                        .. "- Don't elide any code from your output if the answer requires coding.\n"
+                        .. "- Take a deep breath; You've got this!\n",
+                    },
+                }
+            }
+        },
+    }
 
 --
 -- require('lspconfig').racket_langserver.setup()
