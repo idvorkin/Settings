1e1aa8f1cccf725ae8e5fbef83f2ba00602fdcea
Igor Dvorkin <idvorkin@gmail.com>
2024-05-04 18:24:40 -0700

    Checkpoint zsh_include.sh

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -298,4 +298,10 @@ eval "$(mcfly init zsh)"
 eval "$(starship init zsh)"
 eval $(thefuck --alias)
 eval "$(rbenv init -)"
+
+export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
+zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+source <(carapace _carapace)
+
 echo "zsh_include complete"
+
