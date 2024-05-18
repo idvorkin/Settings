24b58dd80c1243c7cbc321f6e078e408590f66b5
Igor Dvorkin <idvorkin@gmail.com>
2024-05-17 19:00:08 -0700

    Updates

--- nvim/nvim_cmp_copilot.lua
+++ nvim/nvim_cmp_copilot.lua
@@ -69,6 +69,7 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+-- K for hover
 
 
 local telescope_builtin = require('telescope.builtin')
