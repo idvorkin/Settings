87ca87cd8669232190200f94216d047334f0bc2b
Igor Dvorkin <idvorkin@gmail.com>
2024-05-18 16:41:41 -0700

    Stop edge re-mapping, add html2text

--- mac/.finicky.js
+++ mac/.finicky.js
@@ -5,28 +5,43 @@ module.exports = {
   rewrite: [
     {
       // Redirect all urls to use https
-      match: ({ url }) => url.protocol === "http", url: { protocol: "https" }
-    }
+      match: ({ url }) => url.protocol === "http",
+      url: { protocol: "http" },
+    },
   ],
   handlers: [
     {
       // Open apple.com and example.org urls in Safari
       match: ["apple.com/*", "example.org/*"],
-      browser: "Safari"
+      browser: "Safari",
     },
     {
       match: ["*gmail.com/*", "*mail.google.com*"],
-      browser: "Microsoft Edge"
+      browser: "Microsoft Edge",
     },
     {
       // Open any url that includes the string "workplace" in Firefox
-        match: ["*.workplace.com/*","*figma*"],
-      browser: "Google Chrome"
+      match: ["*.workplace.com/*", "*figma*"],
+      browser: "Google Chrome",
     },
     {
       // Open any url that includes the string "workplace" in Firefox
-        match: ["fburl.com/*","*internalfb.com/*","*fb.facebook.com/*","*fb.workplace.com/*","*docs.google.com*","*figma*", "*fburl.com/*","*internalfb.com/*","*fb.facebook.com/*","*fb.workplace.com/*", "https://drive.google.com/drive/folders/*", "https://docs.google.com/*", "*fb.quip.com*"],
-      browser: "Google Chrome"
+      match: [
+        "fburl.com/*",
+        "*internalfb.com/*",
+        "*fb.facebook.com/*",
+        "*fb.workplace.com/*",
+        "*docs.google.com*",
+        "*figma*",
+        "*fburl.com/*",
+        "*internalfb.com/*",
+        "*fb.facebook.com/*",
+        "*fb.workplace.com/*",
+        "https://drive.google.com/drive/folders/*",
+        "https://docs.google.com/*",
+        "*fb.quip.com*",
+      ],
+      browser: "Google Chrome",
     },
-  ]
+  ],
 };
