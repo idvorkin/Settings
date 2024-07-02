974986eb4f6533840c86e37b81e962a49a171918
Igor Dvorkin <idvorkin@gmail.com>
2024-07-02 08:23:28 -0700

    Checkpoint y.py

---
+++ py/y.py
@@ -0,0 +1,49 @@
+#!python3
+
+import typer
+import subprocess
+from pathlib import Path
+
+app = typer.Typer(help="A Yabai helper")
+
+_ = """
+
+~/settings/config/yabai/yabairc
+
+"""
+
+
+def call_yabi(prompt):
+    yabi_root = Path("~/homebrew/bin/yabai/").expanduser()
+
+    # Split the prompt to run the path
+    prompt_parts = prompt.split()
+    command = [str(yabi_root)] + prompt_parts
+
+    # Run the command in the remote shell
+    try:
+        out = subprocess.run(command, check=True, capture_output=True, text=True)
+        print(out.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e.stderr}")
+
+
+@app.command()
+def hflip():
+    call_yabi("-m space --mirror y-axis")
+
+
+def restart():
+    call_yabi("--restart-service")
+
+
+def start():
+    call_yabi("--start-service")
+
+
+def stop():
+    call_yabi("--stop-service")
+
+
+if __name__ == "__main__":
+    app()
