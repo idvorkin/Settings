840a8cc50777044bfcb7562e53167c9864f830d0
Igor Dvorkin <idvorkin@gmail.com>
2024-07-31 07:54:11 -0700

    [nvim_plugins.lua] Update to support latest betterescape model

--- nvim/nvim_plugins.lua
+++ nvim/nvim_plugins.lua
@@ -200,10 +200,10 @@ local plugins = {
     {
         "max397574/better-escape.nvim",
         opts = {
-            mapping = {"fj"}, -- a table with mappings to use
-            timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-            clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-            keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+            i = {
+                f = { j = "<Esc>" }
+            }
+
         }
     },
     "stevearc/aerial.nvim",
