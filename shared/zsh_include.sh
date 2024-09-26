2c4f0522082b9c641e2b61e20cd92b22dc3e20c8
Igor Dvorkin <idvorkin@gmail.com>
2024-09-26 07:36:37 -0700

    Upgrade from exa to exa

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -243,7 +243,7 @@ function work_cli()
 # Set alias that are always better
 alias_if_exists cat bat
 alias_if_exists ranger yazi
-alias_if_exists ls exa
+alias_if_exists ls eza
 alias_if_exists df duf
 alias_if_exists top htop
 alias_if_exists ndcu gdu
