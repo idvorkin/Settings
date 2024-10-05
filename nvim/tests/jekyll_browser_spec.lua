d7df1ad0c7a35cb31082dd7114a757cf1eeb528d
Igor Dvorkin <idvorkin@gmail.com>
2024-10-05 09:39:44 -0700

    Start adding tests

---
+++ nvim/tests/jekyll_browser_spec.lua
@@ -0,0 +1,6 @@
+-- Load the nvim_logic file to access the parse_youtube_url function
+local nvim_logic = require("nvim.nvim_logic")
+
+describe("OpenJeykllInBrowser", function()
+	it("should open the correct URL for a Jekyll post", function() end)
+end)
