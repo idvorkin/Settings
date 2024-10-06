5c8355d11b3d27a5abfcdf508bd2dfeafdffacc5
Igor Dvorkin <idvorkin@gmail.com>
2024-10-05 21:38:16 -0700

    Sweet - switch to Lua based markdown toc updater!!

--- nvim/nvim_plugins.lua
+++ nvim/nvim_plugins.lua
@@ -412,8 +412,6 @@ local function setupMarkdown()
 		-- "ixru/nvim-markdown",
 		"preservim/vim-markdown",
 
-		-- Auto Update ToC
-		"mzlogin/vim-markdown-toc",
 		{
 			"MeanderingProgrammer/render-markdown.nvim",
 			opts = {
@@ -452,6 +450,35 @@ local function setupMarkdown()
 				vim.g.mkdp_filetypes = { "markdown" }
 			end,
 			ft = { "markdown" },
+		},
+		-- Auto Update ToC
+		-- "mzlogin/vim-markdown-toc",
+		{
+			"hedyhli/markdown-toc.nvim",
+			ft = "markdown", -- Lazy load on markdown filetype
+			cmd = { "Mtoc" }, -- Or, lazy load on "Mtoc" command
+			opts = {
+				fences = {
+					enabled = true,
+					-- These fence texts are wrapped within "<!-- % -->", where the '%' is
+					-- substituted with the text.
+					start_text = "vim-markdown-toc-start",
+					end_text = "vim-markdown-toc-end",
+					-- An empty line is inserted on top and below the ToC list before the being
+					-- wrapped with the fence texts, same as vim-markdown-toc.
+				},
+				toc_list = {
+					-- string or list of strings (for cycling)
+					-- If cycle_markers = false and markers is a list, only the first is used.
+					-- You can set to '1.' to use a automatically numbered list for ToC (if
+					-- your markdown render supports it).
+					markers = "-",
+					cycle_markers = false,
+					-- Example config for cycling markers:
+					----- markers = {'*', '+', '-'},
+					----- cycle_markers = true,
+				},
+			},
 		},
 	}
 end
