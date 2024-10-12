52cacdc9ce6c51f5b1cb17f1c16d6316385098ff
Igor Dvorkin <idvorkin@gmail.com>
2024-10-12 08:33:47 -0700

    Erase dead files

--- forcepy.sh
+++
@@ -1,4 +0,0 @@
-# Sigh, sometimes you need to fix openssl when you get SSL errors
-#  sudo chown -R $(whoami) /opt/homebrew/Cellar/openssl@1.1 && brew reinstall openssl@1.1 && brew reinstall openssl@1.1
-# pip3 install setuptools_rust
-brew unlink python@3.10 && brew link --overwrite python@3.10
