d4c9575a85f8e0d6a8e0a4c9fa10ea84b8b9d6a7
Igor Dvorkin <idvorkin@gmail.com>
2025-02-02 17:58:21 -0800

    feat(neovim-plugins): enhance git plugin functionality
    **Reason for change**
        * Improve git plugin functionality in neovim
        * Add support for gist creation and management
        * Prevent neovim buffer inception when opening gists
    
    **Details**
        * Added Rawnly/gist.nvim plugin for gist creation and management
        * Added samjwill/nvim-unception plugin to prevent neovim buffer inception
        * Updated git plugin configuration to include new plugins and settings
        * Maintained existing git plugin functionality, including vim-fugitive and
    gitsigns.nvim
    
    commit message generated by llama-3.3-70b-versatile in 0.99 seconds

--- nvim/nvim_plugins.lua
+++ nvim/nvim_plugins.lua
@@ -401,8 +401,22 @@ plugins = appendTables(plugins, { "tpope/vim-surround" })
 ]]
 
 local git_plugins = {
-	"tpope/vim-fugitive",
-	"lewis6991/gitsigns.nvim",
+    "tpope/vim-fugitive",
+    {
+        "Rawnly/gist.nvim",
+        cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+        config = true
+    },
+    -- `GistsList` opens the selected gif in a terminal buffer,
+    -- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
+    -- and prevents neovim buffer inception
+    {
+        "samjwill/nvim-unception",
+        lazy = false,
+        init = function() vim.g.unception_block_while_host_edits = true end
+    },
+
+    "lewis6991/gitsigns.nvim",
 	--  DiffViewOpen
 	"sindrets/diffview.nvim",
 	"NeogitOrg/neogit",
