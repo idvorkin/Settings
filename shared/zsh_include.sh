1fb162948d3ef706ad60e26081a11500031831e0
Igor Dvorkin <idvorkin@gmail.com>
2025-03-15 05:14:03 -0700

    refactor(zsh_include): update exported secrets for IFTTT API and SMS event
    commit message generated by llama-3.3-70b-versatile in 0.21 seconds

--- shared/zsh_include.sh
+++ shared/zsh_include.sh
@@ -389,8 +389,8 @@ function export_secrets()
     esecret_jq LANGCHAIN_API_KEY
     esecret_jq ANTHROPIC_API_KEY
     esecret_jq OPENAI_API_KEY
-    esecret_jq IFTTT_API_KEY
     esecret_jq IFTTT_WEBHOOK_KEY
+    esecret_jq IFTTT_WEBHOOK_SMS_EVENT
     esecret_jq TWILIO_ACCOUNT_SID
     esecret_jq TWILIO_AUTH_TOKEN
     esecret_jq TWILIO_FROM_NUMBER
